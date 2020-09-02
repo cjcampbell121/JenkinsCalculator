d2416dfa4cd07c3631898aaae8beb5d8eee6a153
BuildTools <unconfigured@null.spigotmc.org>
2020-09-01 23:29:18 -0600

    Added createUniqueId functionality

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -72,7 +73,7 @@ class Calculator {
      */
     String createUniqueID(String n){
 
-        return null;
+        return n + UUID.randomUUID().toString();
     }
 
 
