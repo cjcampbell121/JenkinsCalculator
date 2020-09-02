1e89858542a4c0b027093f1aaefc05627373b7f2
BuildTools <unconfigured@null.spigotmc.org>
2020-09-01 23:22:29 -0600

    Add fibonacci

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -39,7 +39,15 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        int fib[] = new int[n+2];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i-2] + fib[i-1];
+        }
+
+        return fib[n];
     }
 
 
