c47ed5c782201bf17bec5c91fb1c9b715bcce8c5
BuildTools <unconfigured@null.spigotmc.org>
2020-09-02 14:16:10 -0600

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -40,15 +40,16 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        int fib[] = new int[n+2];
-        fib[0] = 0;
-        fib[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            fib[i] = fib[i-2] + fib[i-1];
-        }
-
-        return fib[n];
+//        int fib[] = new int[n+2];
+//        fib[0] = 0;
+//        fib[1] = 1;
+//
+//        for (int i = 2; i <= n; i++) {
+//            fib[i] = fib[i-2] + fib[i-1];
+//        }
+//
+//        return fib[n];
+        return 0;
     }
 
 
